2512fc66858410df0be26b2976d1844e5e56a84e
Jose Ramirez <jmramirezr@unal.edu.co>
2019-03-04 18:41:24 -0500

    created module for data validation and created account model with insert function

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,6 +1,11 @@
+require_relative '../models/account'
 require_relative '../../db/db_handler'
 
 #initializes the db with the given name and create it if does not exists
 db_folder_path = "../../db/"
 @dbHandler = DbHandler.new(db_folder_path, "mock_nequi_db.db")
 @dbHandler.create
+
+acc = Account.new(@dbHandler)
+
+acc.insert(1000000, 2000000, "03/04/2019 18:25:00")
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,43 @@
 require_relative '../../db/db_handler'
+require_relative '../modules/validate_data'
 
-#initializes the db with the given name and create it if does not exists
-db_folder_path = "../../db/"
-@dbHandler = DbHandler.new(db_folder_path, "mock_nequi_db.db")
-db_connect = @dbHandler.connect_sqlite
+class Account
+    include ValidateData
+
+    def initialize(db_handler)
+        @db_handler = db_handler
+    end
+
+    def valid_data?(*data)
+        avaiable_balance_valid = numeric_validation(data[0])
+        total_balance_valid = numeric_validation(data[1])
+        creation_date_valid = datetime_validation(data[2])
+
+        if (avaiable_balance_valid and total_balance_valid and \
+            creation_date_valid)
+            return true
+        else
+            return false
+        end
+    end 
+
+    def insert(*data)
+        db = @db_handler.connect
+        
+        avaiable_balance = data[0]
+        total_balance = data[1]
+        creation_date = data[2]
+        
+        if valid_data?(*data)
+            db.execute("INSERT INTO accounts(avaiable_balance, total_balance, 
+                creation_date) VALUES(?, ?, ?)", 
+                [avaiable_balance, total_balance, creation_date])
+        else
+            print("ERROR: couldn't insert account data")
+        end
+    end
+
+end
+
+
 
