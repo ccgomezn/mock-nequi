cf0840f35a40498f0bb5cfd98fe01b8de409002d
Jose Ramirez <jmramirezr7@gmail.com>
2019-03-18 10:43:51 -0500

    changed model names

--- app/models/User.rb
+++
@@ -1,10 +0,0 @@
-class User
-    attr_accessor :name, :email, :password_digest, :account_id
-
-    def initialize(params)
-      @name = params[:name]
-      @email = params[:email]
-      @password_digest = params[:password_digest]
-      @account_id = params[:account_id]
-      end
-end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User
+    attr_accessor :id, :name, :email, :account_id
+
+    def initialize(params)
+      @id = params[:id]
+      @name = params[:name]
+      @email = params[:email]
+      @account_id = params[:account_id]
+    end
+
+end
+
